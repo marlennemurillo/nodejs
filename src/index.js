0e21b572c8e381fcd1adc9d6d26ea6f29f40e29e
MarleneMurillo <marlennemurillo0@gmail.com>
2020-07-22 19:48:26 -0500

    version 1

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//console.log('Server Lisentung!');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//setings
+app.set('port',3000);
+app.set('views',path.join(__dirname,'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','ejs');
+
+//middlewares
+
+
+//routes
+app.use(require('./routes/index'));
+
+//static file
+app.use(express.static(path.join(__dirname,'public')))
+
+
+//listening the server
+app.listen(app.get('port'),() => {
+    console.log('Server on port',app.get('port'));
+});
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    //res.sendFile(path.join(__dirname,'views/index.html'));
+    res.render('index.html', { title: 'First WebSite'});   
+});
+
+
+router.get('/contact',(req,res)=>{
+    res.render('contact.html', { title: 'Contact Page '});   
+});
+
+
+module.exports = router;
